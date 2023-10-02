b70a70b4c337a56557473bf22eb24e21ee855d72
Flo-Bac <florian-bachmann@bluewin.ch>
2023-10-02 06:54:13 -0400

    Progress

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
+@admin.register(CarMake)
+class CarMake(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+@admin.register(CarModel)
+class CarModel(admin.Model.Admin):
+    list_display = ('name', 'carmake', 'type', 'year')
 
 # CarModelInline class
 
